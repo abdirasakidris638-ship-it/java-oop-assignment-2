7b0811ddc92e4c6a945aa928db68c096f068ff06
Tesfamichael-G <tesfamichael.gegziabher@aait.edu.et>
2026-03-24 12:12:35 +0300

    Update CheckingAccount.java

--- src/main/java/com/jju/CheckingAccount.java
+++ src/main/java/com/jju/CheckingAccount.java
@@ -1,1 +1,16 @@
+package com.jju;
+
+public class CheckingAccount extends BankAccount {
+    private static final double TRANSACTION_FEE = 1.50;
+
+  // Student Task: Create constructor and call the superclass constructor and ensure balance is valid
+    
+
+    @Override
+    public void withdraw(double amount) {
+       // implement withdraw logic here
+    }
+
+    
+}
 
